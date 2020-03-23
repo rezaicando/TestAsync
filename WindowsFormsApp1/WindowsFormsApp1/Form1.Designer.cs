d5de69466af8d046ad99af7869a6146fd1236c38
rezaicando <reza.icando@gmail.com>
2020-03-23 00:18:15 -0700

    Sync and Async

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -30,38 +30,49 @@
         {
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button2
             // 
-
-            this.button2.Location = new System.Drawing.Point(294, 207);
+            this.button2.Location = new System.Drawing.Point(576, 23);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(139, 31);
             this.button2.TabIndex = 0;
-            this.button2.Text = "button2";
+            this.button2.Text = "Async Execution";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-
-            this.button1.Location = new System.Drawing.Point(285, 66);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 34);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(139, 31);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Normal execution";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(23, 106);
+            this.txt1.Multiline = true;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(728, 300);
+            this.txt1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +80,7 @@
 
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt1;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,16 +19,104 @@ namespace WindowsFormsApp1
         }
 
 
-        private void button2_Click(object sender, EventArgs e)
+        private async void button2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("from new2");
+            //Async Part
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            //await RundownloadASync();
+            await RundownloadParallelASync();
+
+            watch.Stop();
+            var elapseMs = watch.ElapsedMilliseconds;
+            txt1.Text += $"Total execution time: {elapseMs}";
         }
 
-            private void button1_Click(object sender, EventArgs e)
+        private async Task RundownloadASync()
+        {
+            List<String> websites = PrepData();
+            foreach (string site in websites)
             {
-                MessageBox.Show("from new1");
+                WebsiteDataModel results =await Task.Run(() => DownlaodWebSite(site));
+                ReportWebsiteInfo(results);
+            }
+        }
+
+
+        private async Task RundownloadParallelASync()
+        {
+            List<String> websites = PrepData();
+            List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+            foreach (string site in websites)
+            {
+                //tasks.Add(Task.Run(() => DownlaodWebSite(site)));
+                //tasks.Add(Task.Run(() => DownlaodWebSiteAsync(site)));
+                tasks.Add(DownlaodWebSiteAsync(site));
+            }
+
+            WebsiteDataModel[] results = await Task.WhenAll(tasks);
+
+            results.ToList< WebsiteDataModel>().ForEach(p => ReportWebsiteInfo(p));
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            RundownloadSync();
+            watch.Stop();
+            var elapseMs = watch.ElapsedMilliseconds;
+            txt1.Text += $"Total execution time: {elapseMs}";
+
+        }
+
+        private void RundownloadSync()
+        {
+            List<String> websites = PrepData();
+            foreach(string site in websites)
+            {
+                WebsiteDataModel results = DownlaodWebSite(site);
+                ReportWebsiteInfo(results);
 
             }
         }
+
+        private void ReportWebsiteInfo(WebsiteDataModel data)
+        {
+            txt1.Text += $"{data.WebsiteUrl}  downloaded: {data.WebsiteData.Length} characters long. {Environment.NewLine}";
+        }
+
+
+        private async Task<WebsiteDataModel> DownlaodWebSiteAsync(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = await client.DownloadStringTaskAsync(websiteURL);
+            return output;
+        }
+
+        private WebsiteDataModel DownlaodWebSite(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = client.DownloadString(websiteURL);
+            return output;
+        }
+
+        private List<string> PrepData()
+        {
+            List<String> output = new List<string>();
+            txt1.Text = "";
+            output.Add("https://www.yahoo.com");
+            output.Add("https://www.google.com");
+            output.Add("https://www.cnn.com");
+            output.Add("https://www.codeproject.com");
+            output.Add("https://www.stackoverflow.com");
+            output.Add("https://www.linkedin.com/feed/");
+            return output;
+        }
     }
+}
 
