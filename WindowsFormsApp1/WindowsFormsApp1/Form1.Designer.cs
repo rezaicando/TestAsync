c749a1a2e22827afdb4d2e19ffbf3646d4ce8bb5
rezaicando <reza.icando@gmail.com>
2020-03-28 21:58:57 -0700

    Minor Changes

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(576, 23);
+            this.button2.Location = new System.Drawing.Point(612, 49);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(139, 31);
             this.button2.TabIndex = 0;
@@ -45,7 +45,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(26, 34);
+            this.button1.Location = new System.Drawing.Point(23, 49);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(139, 31);
             this.button1.TabIndex = 0;
@@ -65,12 +65,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(780, 450);
             this.Controls.Add(this.txt1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Async vs Sync Download Speed";
             this.ResumeLayout(false);
             this.PerformLayout();
 
